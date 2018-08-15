10b3a6c4168e9ccc8fbb7d39ee1f00c0c54b3ca4
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-08-15 21:22:43 +0500

    fix: story component

--- src/components/Story.js
+++ src/components/Story.js
@@ -6,7 +6,7 @@ const Story = ({ item }) => {
       <div><a href={item.url}>{item.title}</a></div>
       <div>Author: {item.by}</div>
       <div>Score: {item.score}</div>
-      <div>No of Comments: {item.kids.length}</div>
+      <div>No of Comments: {item.kids? item.kids.length: 0}</div>
     </div>
   )
 }
