847275941c38d458f0c4f007587ff71b54e70480
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-08-15 21:46:21 +0500

    update story component

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Card, CardBody, CardTitle} from 'reactstrap';
 
 const Story = ({ item }) => {
   return (
-    <div>
-      <div><a href={item.url}>{item.title}</a></div>
-      <div>Author: {item.by}</div>
-      <div>Score: {item.score}</div>
-      <div>No of Comments: {item.kids? item.kids.length: 0}</div>
-    </div>
+    <Card onClick={()=> item.url? window.open(item.url, "_blank"): null}>
+      <CardBody>
+        <CardTitle>{item.title}</CardTitle>
+        <div>Author: {item.by}</div>
+        <div>Score: {item.score}</div>
+        <div>No of Comments: {item.kids? item.kids.length: 0}</div>
+      </CardBody>
+    </Card>
   )
 }
 
